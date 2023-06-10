24c974f73548256834c7c658f01373a6aa7769de
kayoJr <kalebmelaku97@gmail.com>
2023-06-10 16:55:02 +0300

    updated

--- backend/db.php
+++ backend/db.php
@@ -2,7 +2,7 @@
 $server = "localhost";
 $username = "root";
 $password = "";
-$db_name = "drhibists";
+$db_name = "drhibist";
 
 $conn = mysqli_connect($server, $username, $password, $db_name);
 $con = mysqli_connect($server, $username, $password, $db_name);
