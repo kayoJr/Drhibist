4e9f9360eedf59316dc997baec3058685df07e27
kalebmelaku <kalebmelaku97@hotmail.com>
2024-06-03 09:55:42 +0300

    ultrasound payment fix

--- backend/remove_ultra.php
+++ backend/remove_ultra.php
@@ -1,13 +1,3 @@
-
-// require "db.php";
-// $id = $_GET['id'];
-// $delete = $conn->query("DELETE FROM `income` WHERE `id` = '$id'");
-// $delete = $conn->query("DELETE FROM `ultraqueue` WHERE `ultraId` = '$id'");
-// if ($delete) {
-//     header("Location: ../Users/Admin/index.php?msg=Payment Deleted");
-// }
-
-
 <?php
 require "db.php";
 $id = $_GET['id'];
